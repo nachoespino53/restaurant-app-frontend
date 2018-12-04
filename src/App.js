e9b28482ca97ee565bf4fee3bcd3e50080f73ce7
nachoespino53 <nachoespino53@gmail.com>
2018-12-04 14:05:44 -0500

    Added NavBar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './containers/NavBar';
+// import Bulma from 'react-bulma-components/full';
+import 'bulma/css/bulma.css'
 
 class App extends Component {
   render() {
     return (
-      <div>This is the app container</div>
+      <div>
+      <NavBar />
+      <br/>
+      <div className="columns is-variable is-5">
+          <div className="column  has-background-grey-lighter"></div>
+          <div className="column is-half  has-background-grey-lighter"></div>
+          <div className="column  has-background-grey-lighter"></div>
+      </div>
+      </div>
     );
   }
 }
