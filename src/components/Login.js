ce82b39c05a4ab14ffa816b9a496ec76fa808178
nachoespino53 <nachoespino53@gmail.com>
2019-01-14 15:33:07 -0500

    Added Login Page

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,9 +2,44 @@ import React, { Component } from 'react';
 
 export default class Login extends Component {
 
+    state = {
+        username: "",
+        password: ""
+    }
+
     render () {
         return (
-            <div>Login</div>
+            <section className="hero is-fullheight">
+                <div className="hero-body">
+                    <div className="container has-text-centered">
+                        <div className="column is-4 is-offset-4">
+                            <div className="box">
+                                <h3 className="title has-text-grey">Login</h3>
+                                <p className="subtitle has-text-grey">Please login to proceed.</p>
+                                    <form>
+                                        <div className="field">
+                                            <div className="control">
+                                                <input className="input is-large" type="email" placeholder="Your Email" autoFocus="" />
+                                            </div>
+                                        </div>
+
+                                            <div className="field">
+                                                <div className="control">
+                                                    <input className="input is-large" type="password" placeholder="Your Password" />
+                                                </div>
+                                                </div>
+                                                <div className="field">
+                                                    <label className="checkbox">
+                                                        <input type="checkbox" /> Remember me
+                                                    </label>
+                                                </div>
+                                                    <button className="button is-block is-info is-large is-fullwidth">Login</button>
+                                    </form>
+                            </div>
+                                        </div>
+            </div>
+        </div>
+    </section>
         )
     }
 }  
